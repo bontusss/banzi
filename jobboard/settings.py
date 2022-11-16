31eb1adcc216ef3d3c023ab4b9debd7493c17016
bontusss <ikwecheghu@gmail.com>
2022-11-16 13:58:47 +0100

    All basic funcs added. Email not working in prod

--- jobboard/settings.py
+++ jobboard/settings.py
@@ -29,9 +29,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'hfugvgufkuyofyfuftyyyrc!456789jlknkljbkh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['bontusmayor.pythonanywhere.com']
 
 
 # Application definition
